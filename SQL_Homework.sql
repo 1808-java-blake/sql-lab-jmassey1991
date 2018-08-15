a6f8e8a69398d12523790a03e76b1079c3a727c3
Justin Massey <jmassey_1991@hotmail.com>
2018-08-14 23:15:29 -0400

    Did some of the homework

--- SQL_Homework.sql
+++ SQL_Homework.sql
@@ -1,5 +1,5 @@
 -- Part I – Working with an existing database
-
+SET SCHEMA 'chinook'; -- FOR THE WHOLE THANG
 -- 1.0	Setting up Oracle Chinook
 -- In this section you will begin the process of working with the Oracle Chinook database
 -- Task – Open the Chinook_Oracle.sql file and execute the scripts within.
@@ -7,26 +7,62 @@
 -- In this section you will be performing various queries against the Oracle Chinook database.
 -- 2.1 SELECT
 -- Task – Select all records from the Employee table.
+SELECT * FROM Employee;
 -- Task – Select all records from the Employee table where last name is King.
+SELECT * FROM Employee
+WHERE lastname = 'King';
 -- Task – Select all records from the Employee table where first name is Andrew and REPORTSTO is NULL.
+SELECT * FROM Employee
+WHERE firstname = 'Andrew'
+AND reportsto is NULL;
 -- 2.2 ORDER BY
 -- Task – Select all albums in Album table and sort result set in descending order by title.
+SELECT * FROM Album
+ORDER BY title DESC;
 -- Task – Select first name from Customer and sort result set in ascending order by city
+SELECT firstname 
+FROM customer 
+ORDER BY city ASC;
 -- 2.3 INSERT INTO
 -- Task – Insert two new records into Genre table
+INSERT INTO Genre (genreid, "name")
+VALUES (26, 'Electro Pop'), (27, 'Vapor Soul');
 -- Task – Insert two new records into Employee table
+INSERT INTO employee 
+VALUES (9, 'blob', 'boy', 'Trash N Stuff', 1, '1989-04-22', '2005-09-30', '123 No Where', 'HateCity', 'HelpMe', 'Cambodia', '6969', '903-227-2834', '903-345-3458', 'blobboy@chinookcorp.com'),
+(10, 'blob', 'boy', 'Trash N Stuff', 1, '1989-04-22', '2005-09-30', '123 No Where', 'HateCity', 'HelpMe', 'Cambodia', '6969', '903-227-2834', '903-345-3458', 'blobboy@chinookcorp.com');
 -- Task – Insert two new records into Customer table
+INSERT INTO customer
+VALUES (60, 'Hate', 'Fear', 'Plumbus', '321 YesDaddy', 'PlagueCity', 'TX', 'US and A', '34543', '903-234-2345', '456-457-2334', 'grabme@gmail.com', 5),
+(61, 'Hate', 'Fear', 'Plumbus', '321 YesDaddy', 'PlagueCity', 'TX', 'US and A', '34543', '903-234-2345', '456-457-2334', 'grabme@gmail.com', 5);
 -- 2.4 UPDATE
 -- Task – Update Aaron Mitchell in Customer table to Robert Walter
+UPDATE customer 
+SET firstname = 'Robert', lastname = 'Walter'
+WHERE firstname = 'Aaron' AND lastname = 'Mitchell';
 -- Task – Update name of artist in the Artist table “Creedence Clearwater Revival” to “CCR”
+UPDATE artist
+SET "name" = 'CCR'
+WHERE "name" = 'Creedence Clearwater Revival';
 -- 2.5 LIKE
 -- Task – Select all invoices with a billing address like “T%”
+SELECT * FROM invoice
+WHERE billingaddress LIKE 'T%';
 -- 2.6 BETWEEN
 -- Task – Select all invoices that have a total between 15 and 50
+SELECT * FROM invoice
+WHERE total BETWEEN 15 AND 50;
 -- Task – Select all employees hired between 1st of June 2003 and 1st of March 2004
+SELECT * FROM employee
+WHERE hiredate BETWEEN '2003-06-01' AND '2004-03-01';
 -- 2.7 DELETE
 -- Task – Delete a record in Customer table where the name is Robert Walter (There may be constraints that rely on this, find out how to resolve them).
-
+DELETE FROM invoiceline 
+WHERE invoiceid in (SELECT invoiceid FROM invoice WHERE customerid IN (SELECT customerid FROM customer WHERE firstname = 'Robert' AND lastname = 'Walter'));
+DELETE FROM invoice
+WHERE customerid IN (SELECT customerid FROM customer WHERE firstname = 'Robert' AND lastname = 'Walter');
+DELETE FROM customer 
+WHERE firstname = 'Robert' AND lastname = 'Walter';
 -- 3.0	SQL Functions
 -- In this section you will be using the Oracle system functions, as well as your own functions, to perform various actions against the database
 -- 3.1 System Defined Functions
